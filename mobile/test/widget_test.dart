7d5c4a3e7210587c763da55c4154c41ba57ebcdc
camping-joe <81806866@qq.com>
2026-05-01 12:46:24 +0800

    Simplify Flutter widget test to avoid pumpAndSettle timeout

--- mobile/test/widget_test.dart
+++ mobile/test/widget_test.dart
@@ -1,11 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flashcard_mobile/main.dart';
 
 void main() {
-  testWidgets('App renders without error', (WidgetTester tester) async {
-    // 应用能正常构建即视为通过
+  testWidgets('App builds without throwing', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-    await tester.pumpAndSettle();
   });
 }
